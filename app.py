3e2eb3546097c878be52abc591c74fc29e88ee9b
Alex Yancey <me@alexyancey.com>
2023-07-27 20:44:26 -0700

    hardcode creds

--- app.py
+++ app.py
@@ -8,8 +8,13 @@ import uuid
 import time
 from flask_cors import CORS, cross_origin
 
-s3 = boto3.resource("s3")
 
+session = boto3.Session(
+    aws_access_key_id="***REMOVED***",
+    aws_secret_access_key="***REMOVED***",
+)
+
+s3 = session.resource("s3")
 
 
 prepared_videos = {}
